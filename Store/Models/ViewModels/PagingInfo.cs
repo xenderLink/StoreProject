d6428dece1d7055c2ddea0904c83643e250f903f
xenderLink <93864905+xenderLink@users.noreply.github.com>
2023-03-30 23:06:16 +0500

    Update PagingInfo.cs

--- Store/Models/ViewModels/PagingInfo.cs
+++ Store/Models/ViewModels/PagingInfo.cs
@@ -5,9 +5,8 @@ public class PagingInfo
     public int TotalProducts {get;set;} //Всего продуктов в базе
     public int ProductsPerPage {get;set;} //Количество продуктов на страничке
     public int CurrentPage {get;set;} //Текущая страничка
-    public int TotalPages() => 
-    (int) Math.Ceiling( (decimal)TotalProducts/ProductsPerPage); //Всего страниц = Количество продуктов/Количество продуктов на страничке
-    public static int TotalPages(int TotalProducts, int ProductsPerPage) =>     
-    (int) Math.Ceiling( (decimal)TotalProducts/ProductsPerPage);
+   
+    public int TotalPages() => (int) Math.Ceiling( (decimal)TotalProducts/ProductsPerPage); //Всего страниц = Количество продуктов/Количество продуктов на страничке
     
+    public static int TotalPages(int TotalProducts, int ProductsPerPage) => (int) Math.Ceiling( (decimal)TotalProducts/ProductsPerPage);
 }
