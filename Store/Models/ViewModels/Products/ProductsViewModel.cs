eb34e066e24bfe366d7d646268f2b21287a42d62
xenderLink <93864905+xenderLink@users.noreply.github.com>
2023-03-30 23:05:14 +0500

    Update ProductsViewModel.cs

--- Store/Models/ViewModels/Products/ProductsViewModel.cs
+++ Store/Models/ViewModels/Products/ProductsViewModel.cs
@@ -1,11 +1,10 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Store.Models.ViewModels;
+
 public class ProductsViewModel
 {
     public IEnumerable<Product> Products {get;set;} = Enumerable.Empty<Product>();
     public Dictionary <int, string> Categories {get;set;} 
     public PagingInfo PagingInfo {get;set;} = new ();
-   
-
 }
