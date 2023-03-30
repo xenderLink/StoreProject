9a5b34e2aa4c948f5a010904f325219bf2356e04
xenderLink <heihachi1337mishima@gmail.com>
2023-03-30 22:56:20 +0500

    Выравнивание

--- Store/Infrastructure/PageLinkTagHelper.cs
+++ Store/Infrastructure/PageLinkTagHelper.cs
@@ -3,12 +3,12 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+
 using Store.Models.ViewModels;
 
 ///<sumary>
 ///Пагинация
 ///</sumary>
-
 
 namespace Store.Infrasctructure;
 
@@ -24,7 +24,6 @@ public class PageLinkTagHelper : TagHelper
 
     [ViewContext]
     [HtmlAttributeNotBound]
-
     public ViewContext? ViewContext {get;set;}
     public PagingInfo? PageModel {get; set;}
     public string? PageAction {get; set;}
@@ -35,29 +34,30 @@ public class PageLinkTagHelper : TagHelper
     public string PageClassNormal {get; set;} = String.Empty;
     public string PageClassSelected {get; set;} = String.Empty;
 
-
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
-        if ( ViewContext!=null && PageModel !=null )
+        if(ViewContext != null && PageModel != null)
         {
             IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
             TagBuilder div = new TagBuilder("div");
             
-            for (int i = 1; i<=PageModel.TotalPages(); i++)
+            for(int i = 1; i<=PageModel.TotalPages(); i++)
             {
                 TagBuilder anchor = new TagBuilder("a");
                 PageUrlValues["productPage"] = i;
                 
                 anchor.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
                 
-                if (PageClassesEnabled) {
-                        anchor.AddCssClass(PageClass);
-                        anchor.AddCssClass(i == PageModel.CurrentPage
-                            ? PageClassSelected : PageClassNormal);
-                    }
+                if(PageClassesEnabled)
+                {
+                    anchor.AddCssClass(PageClass);
+                    anchor.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
+
                 anchor.InnerHtml.Append(i.ToString());
                 div.InnerHtml.AppendHtml(anchor);
             }
+
             output.Content.AppendHtml(div.InnerHtml);
         }
     }
