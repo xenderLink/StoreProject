62e4394f1d3ad3ff96c04d16ec294a256754474d
xenderLink <93864905+xenderLink@users.noreply.github.com>
2023-03-30 23:03:32 +0500

    Update LoginViewModel.cs

--- Store/Models/ViewModels/Account/LoginViewModel.cs
+++ Store/Models/ViewModels/Account/LoginViewModel.cs
@@ -12,5 +12,6 @@ public class LoginViewModel
     [Required(ErrorMessage = "Введите пароль")]
     [DataType(DataType.Password)]
     public string? Password {get; set;}
+    
     public string ReturnUrl { get; set; } = "/";
 }
